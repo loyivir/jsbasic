a5f72b99732c6a81a1baef4e72cc0bfb40db35ee
loyivir <loyivir@gmail.com>
2021-04-05 00:16:12 +0300

    feat: Add values

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 1000;
+let income = "фриланс";
+let addExpenses = "Интернет, Такси, Коммуналка";
+let deposit = true;
+let mission = 20000;
+let period = 10;
+let budgetDay = money / 30;
 
-alert("Сообщение с любым текстом");
-console.log("Сообщение с любым текстом в консоли");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей/долларов/гривен/юани");
+console.log(addExpenses.toLowerCase().split(", "));
+console.log("Дневной бюджет: " + budgetDay);
