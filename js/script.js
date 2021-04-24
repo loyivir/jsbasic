4afe2274fd7acba51eb38e7c42a73596aec75162
loyivir <loyivir@gmail.com>
2021-04-24 09:58:06 +0300

    feat: Add menu and popup handling

--- js/script.js
+++ js/script.js
@@ -26,10 +26,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
       if (timer.timeRemaining > 0 && !isIntervalSet) {
         intervalId = setInterval(updateClock, 1000);
-        console.log('Set!');
+
         isIntervalSet = true;
       } else if (timer.timeRemaining <= 0) {
-        console.log('Cleared!');
         clearInterval(intervalId);
         isIntervalSet = false;
         timerHours.textContent = '00';
@@ -43,4 +42,59 @@ window.addEventListener('DOMContentLoaded', () => {
     updateClock();
   }
   countTimer('April 23, 2021 23:28:00');
+
+  // Меню
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = menu.querySelectorAll('ul>li');
+    const handlerMenu = () => {
+      // menu.classList.toggle('active-menu');
+      if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+        menu.style.transform = `translate(0)`;
+      } else {
+        menu.style.transform = `translate(-100%)`;
+      }
+    };
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+  };
+
+  toggleMenu();
+
+  //popup
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+      popupBtn = document.querySelectorAll('.popup-btn'),
+      popupClose = document.querySelector('.popup-close');
+    popup.style.opacity = '0%';
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        if (screen.width < 768) {
+          popup.style.opacity = '100%';
+          popup.style.display = 'block';
+          return;
+        }
+        let start = Date.now();
+        popup.style.display = 'block';
+        let requestId = setInterval(() => {
+          let timePassed = Date.now() - start;
+
+          popup.style.opacity = `${(timePassed / 10).toFixed(0)}%`;
+          if (timePassed >= 1000) {
+            clearInterval(requestId);
+            return;
+          }
+        }, 10);
+      });
+    });
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+      popup.style.opacity = '0%';
+    });
+  };
+
+  togglePopUp();
 });
